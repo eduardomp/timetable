d76f1895a4afb337512374975b7e64654f1f8638
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2022-04-15 03:36:06 +0100

    adjusting layout

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import PySimpleGUI as sg
 
+zero_pad = ((0, 0), (0, 0))
+
 
 def generate_row(row_num, row_data):
     return [
         sg.Text("         "),
         sg.Button('', size=(12, 4), key=(
-            f"-cell-{row_num}-1-"), pad=(0, 0)),
+            f"-cell-{row_num}-1-"), pad=zero_pad),
+        sg.VSeparator(pad=zero_pad),
         sg.Button('', size=(12, 4), key=(
-            f"-cell-{row_num}-2-"), pad=(0, 0)),
+            f"-cell-{row_num}-2-"), pad=zero_pad),
+        sg.VSeparator(pad=zero_pad),
         sg.Button('', size=(12, 4), key=(
-            f"-cell-{row_num}-3-"), pad=(0, 0)),
+            f"-cell-{row_num}-3-"), pad=zero_pad),
+        sg.VSeparator(pad=zero_pad),
         sg.Button('', size=(12, 4), key=(
-            f"-cell-{row_num}-4-"), pad=(0, 0)),
+            f"-cell-{row_num}-4-"), pad=zero_pad),
+        sg.VSeparator(pad=zero_pad),
         sg.Button('', size=(12, 4), key=(
-            f"-cell-{row_num}-5-"), pad=(0, 0))
+            f"-cell-{row_num}-5-"), pad=zero_pad),
+
     ]
 
 
 def generate_rows():
-    rows = [[sg.Text("Timetable")]]
+    rows = [
+        [sg.Text("Timetable")],
+        [sg.Text("         "), sg.Text("Monday", size=(12, 2)), sg.Text("Tuesday", size=(12, 2)), sg.Text(
+            "Wednesday", size=(12, 2)), sg.Text("Thursday", size=(12, 2)), sg.Text("Friday", size=(12, 2))],
+    ]
     for i in range(0, 24):
-        rows.append([sg.Text(f"{i}:00"), sg.HSeparator()]),
+        rows.append([sg.Text(f"{i}:00", pad=((0, 10), (0, 0))),
+                    sg.HSeparator(pad=zero_pad)]),
         rows.append(generate_row(i, []))
     return rows
 
@@ -50,6 +62,7 @@ layout = [[
     sg.Column(column_right,
               expand_y=True,
               expand_x=True,
+              pad=zero_pad,
               scrollable=True,
               vertical_scroll_only=True,
               size=(890, 680))
